1194e6759d8d46350a815284002dd7bf5d816256
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-04-14 01:27:53 +0200

    move code

--- src/main/java/org/jonnyzzz/stack/StackLine.java
+++ src/main/java/org/jonnyzzz/stack/StackLine.java
@@ -1,19 +1,17 @@
 package org.jonnyzzz.stack;
 
 import com.sun.istack.internal.NotNull;
+import org.jonnyzzz.stack.impl.GeneratedClassLoader;
 import org.jonnyzzz.stack.impl.InternalAction;
 import org.jonnyzzz.stack.impl.NamedExecutor;
-import org.jonnyzzz.stack.impl.NamedExecutorGenerator;
-
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
  */
 public class StackLine {
-    public static <E extends Throwable> void stackLine(@NotNull final String name,
-                                                       @NotNull final Class<E> exception,
-                                                       @NotNull final UnderStackAction<E> fun) throws E {
+    public static <E extends Throwable> void stackLine(final String name,
+                                                       final Class<E> exception,
+                                                       final UnderStackAction<E> fun) throws E {
 
         try {
             executor(name).execute(new InternalAction() {
@@ -32,9 +30,9 @@ public class StackLine {
     }
 
 
-    public static <R, E extends Throwable> void stackLine(@NotNull final String name,
-                                                          @NotNull final Class<E> exception,
-                                                          @NotNull final UnderStackFunction<R, E> fun) throws E {
+    public static <R, E extends Throwable> void stackLine(final String name,
+                                                          final Class<E> exception,
+                                                          final UnderStackFunction<R, E> fun) throws E {
         try {
             executor(name).execute(new InternalAction() {
                 public Object execute() throws Throwable {
@@ -66,46 +64,5 @@ public class StackLine {
     }
 
     private static final GeneratedClassLoader myLoader = new GeneratedClassLoader();
-    private static class GeneratedClassLoader extends ClassLoader {
-        private final AtomicInteger myCounter = new AtomicInteger();
-        private GeneratedClassLoader() {
-            super(NamedExecutor.class.getClassLoader());
-        }
-
-        public NamedExecutor generate(@NotNull final String name) {
-            final String clazz = "com.jonnyzzz.generated.names.generated_class_" + myCounter.incrementAndGet();
-            final byte[] bytes = NamedExecutorGenerator.generateWrapper(clazz, name);
-
-            definePackage(clazz);
-            final Class<?> aClass = defineClazz(clazz, bytes);
-
-            try {
-                return (NamedExecutor) aClass.newInstance();
-            } catch (Throwable e) {
-                throw new RuntimeException("Failed to generate class for name: " + name + ". " + e.getMessage(), e);
-            }
-        }
-
-        @NotNull
-        private Class<?> defineClazz(@NotNull final String name, @NotNull final byte[] bytes) {
-            return defineClass(name, bytes, 0, bytes.length);
-        }
-
-        private void definePackage(@NotNull final String name) {
-            final int i = name.lastIndexOf('.');
-            if (i >= 0) {
-                final String pkgName = name.substring(0, i);
-                // Check if package already loaded.
-                final Package pkg = getPackage(pkgName);
-                if (pkg == null) {
-                    try {
-                        definePackage(pkgName, null, null, null, null, null, null, null);
-                    } catch (IllegalArgumentException e) {
-                        // do nothing, package already defined by some other thread
-                    }
-                }
-            }
-        }
-    }
 
 }
---
+++ src/main/java/org/jonnyzzz/stack/impl/GeneratedClassLoader.java
@@ -0,0 +1,50 @@
+package org.jonnyzzz.stack.impl;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+* @author Eugene Petrenko (eugene.petrenko@gmail.com)
+*/
+public class GeneratedClassLoader extends ClassLoader {
+    private final AtomicInteger myCounter = new AtomicInteger();
+    public GeneratedClassLoader() {
+        super(NamedExecutor.class.getClassLoader());
+    }
+
+    public NamedExecutor generate(@NotNull final String name) {
+        final String clazz = "com.jonnyzzz.generated.names.generated_class_" + myCounter.incrementAndGet();
+        final byte[] bytes = NamedExecutorGenerator.generateWrapper(clazz, name);
+
+        definePackage(clazz);
+        final Class<?> aClass = defineClazz(clazz, bytes);
+
+        try {
+            return (NamedExecutor) aClass.newInstance();
+        } catch (Throwable e) {
+            throw new RuntimeException("Failed to generate class for name: " + name + ". " + e.getMessage(), e);
+        }
+    }
+
+    @NotNull
+    private Class<?> defineClazz(@NotNull final String name, @NotNull final byte[] bytes) {
+        return defineClass(name, bytes, 0, bytes.length);
+    }
+
+    private void definePackage(@NotNull final String name) {
+        final int i = name.lastIndexOf('.');
+        if (i >= 0) {
+            final String pkgName = name.substring(0, i);
+            // Check if package already loaded.
+            final Package pkg = getPackage(pkgName);
+            if (pkg == null) {
+                try {
+                    definePackage(pkgName, null, null, null, null, null, null, null);
+                } catch (IllegalArgumentException e) {
+                    // do nothing, package already defined by some other thread
+                }
+            }
+        }
+    }
+}
