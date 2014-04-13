d6e0f511287e45853c8b0e046ebe073459507e04
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-04-14 00:46:23 +0200

    bytecode utf8 support + test

---
+++ src/main/java/org/jonnyzzz/stack/impl/UTF.java
@@ -0,0 +1,29 @@
+package org.jonnyzzz.stack.impl;
+
+import java.util.Arrays;
+
+/**
+ * @author Eugene Petrenko (eugene.petrenko@gmail.com)
+ */
+public class UTF {
+
+    public static byte[] encode(String str) {
+        final byte[] result = new byte[6 * 64 * 1024]; //class-file strings are limited with 64k
+        int cnt = 0;
+        for (char c : str.toCharArray()) {
+            final int z = (int) c;
+            if (c >= '\u0001' && c <= '\u007f') {
+                result[cnt++] = (byte) c;
+            } else if (c == '\u0000' || (c >= '\u0080' && c <= '\u07FF')) {
+                result[cnt++] = (byte) (0xc0 | (0x1f & (z >> 6)));
+                result[cnt++] = (byte) (0x80 | (0x3f & z));
+            } else {
+                result[cnt++] = (byte) ( 0xe0 | (0x0f & (z >>12)));
+                result[cnt++] = (byte) ( 0x80 | (0x3f & (z>>6)));
+                result[cnt++] = (byte) ( 0x80 | (0x3f & z));
+            }
+        }
+        return Arrays.copyOf(result, cnt);
+    }
+
+}
