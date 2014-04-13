82ca41134c5dbe6d0bc456af42be7094c603b989
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-04-14 01:31:47 +0200

    more functions + cleanup

--- src/main/java/org/jonnyzzz/stack/StackLine.java
+++ src/main/java/org/jonnyzzz/stack/StackLine.java
@@ -1,9 +1,10 @@
 package org.jonnyzzz.stack;
 
-import com.sun.istack.internal.NotNull;
 import org.jonnyzzz.stack.impl.GeneratedClassLoader;
 import org.jonnyzzz.stack.impl.InternalAction;
 import org.jonnyzzz.stack.impl.NamedExecutor;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
@@ -12,7 +13,6 @@ public class StackLine {
     public static <E extends Throwable> void stackLine(final String name,
                                                        final Class<E> exception,
                                                        final UnderStackAction<E> fun) throws E {
-
         try {
             executor(name).execute(new InternalAction() {
                 public Object execute() throws Throwable {
@@ -26,6 +26,20 @@ public class StackLine {
             } else {
                 throw new RuntimeException(throwable);
             }
+        }
+    }
+
+    public static void stackLine(final String name, final Runnable fun) {
+
+        try {
+            executor(name).execute(new InternalAction() {
+                public Object execute() throws Throwable {
+                    fun.run();
+                    return null;
+                }
+            });
+        } catch (Throwable throwable) {
+            throw new RuntimeException(throwable);
         }
     }
 
@@ -57,12 +71,19 @@ public class StackLine {
         R execute() throws E;
     }
 
+    private static NamedExecutor executor(final String name) {
+        //lazy & non-blocking cache created executors to avoid too
+        //may classes from loading
 
-    @NotNull
-    private static NamedExecutor executor(@NotNull final String name) {
-        return myLoader.generate(name);
+        final NamedExecutor executor = myLazyCache.get(name);
+        if (executor != null) return executor;
+
+        final NamedExecutor newExecutor = myLoader.generate(name);
+        final NamedExecutor oldExecutor = myLazyCache.putIfAbsent(name, newExecutor);
+        return oldExecutor != null ? oldExecutor : newExecutor;
     }
 
     private static final GeneratedClassLoader myLoader = new GeneratedClassLoader();
+    private static final ConcurrentHashMap<String, NamedExecutor> myLazyCache = new ConcurrentHashMap<String, NamedExecutor>();
 
 }
