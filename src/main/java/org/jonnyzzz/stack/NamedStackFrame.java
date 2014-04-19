0ee15491bd4935710b79d0208970bd39d326b4f3
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-04-20 01:31:24 +0200

    fix compilation for 1.8

--- src/main/java/org/jonnyzzz/stack/NamedStackFrame.java
+++ src/main/java/org/jonnyzzz/stack/NamedStackFrame.java
@@ -74,10 +74,10 @@ public abstract class NamedStackFrame {
      * Allows to re-use all generated NamedStackFrame proxies
      * for all used names. Caches are stored in static variable
      * and never cleaned up.
-     * <br />
+     *
      * This method could be used when app possible used method names
      * set is limited
-     * <br />
+     *
      * Too many different names may consume to much of perm-gen space
      *
      * @return globally-cached factory instance
@@ -92,7 +92,7 @@ public abstract class NamedStackFrame {
      * This instance of the factory does not have static caches.
      * Meanwhile all proxies for each name are cached and could
      * only be cleaned up with the object instance
-     * <br />
+     *
      * Generated proxies cleanup depends upon JVM classes unloading
      *
      * @return fresh instance
