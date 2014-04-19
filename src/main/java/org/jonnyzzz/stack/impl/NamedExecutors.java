6863e5464ef9b3ff5d985851281cc247d2177027
Eugene Petrenko <eugene.petrenko@gmail.com>
2014-04-19 23:02:32 +0200

    use right logger

--- src/main/java/org/jonnyzzz/stack/impl/NamedExecutors.java
+++ src/main/java/org/jonnyzzz/stack/impl/NamedExecutors.java
@@ -1,12 +1,13 @@
 package org.jonnyzzz.stack.impl;
 
-import com.sun.istack.internal.logging.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jonnyzzz.stack.NamedStackFrame;
 import org.jonnyzzz.stack.impl.gen.NamedExecutorsGenerator;
 import org.jonnyzzz.stack.impl.gen.jcl.JavaClassLoaderGenerator;
 import org.jonnyzzz.stack.impl.gen.stub.EmptyExecutorsGenerator;
 import org.jonnyzzz.stack.impl.gen.stub.StubExecutorImpl;
+
+import java.util.logging.Logger;
 
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
@@ -18,7 +19,7 @@ public class NamedExecutors implements NamedExecutorsGenerator {
         try {
             return new JavaClassLoaderGenerator();
         } catch (Throwable t) {
-            Logger.getLogger(NamedStackFrame.class).warning("Failed to initialize proxy for NamedStackFrame. " + t.getMessage() + ". Default proxy will be used", t);
+            logger().warning("Failed to initialize proxy for NamedStackFrame. " + t.getMessage() + ". Default proxy will be used");
             return new EmptyExecutorsGenerator();
         }
     }
@@ -29,8 +30,13 @@ public class NamedExecutors implements NamedExecutorsGenerator {
             return myLoader.generate(name);
         } catch (Throwable t) {
             //failed to generate the proxy, so we fallback
-            Logger.getLogger(NamedStackFrame.class).warning("Failed to generate proxy for NamedStackFrame with name '" + name + "'. " + t.getMessage() + ". Default proxy will be used", t);
+            logger().warning("Failed to generate proxy for NamedStackFrame with name '" + name + "'. " + t.getMessage() + ". Default proxy will be used");
             return new StubExecutorImpl();
         }
+    }
+
+    @NotNull
+    private Logger logger() {
+        return Logger.getLogger(NamedStackFrame.class.getName());
     }
 }
