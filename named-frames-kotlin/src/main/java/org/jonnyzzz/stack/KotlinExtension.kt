85fc12a88654bd13c973d31a364492cefc364ec5
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-03-12 12:39:58 +0100

    drop Kotlin library. No sense for it

--- named-frames-kotlin/src/main/java/org/jonnyzzz/stack/KotlinExtension.kt
+++
@@ -1,7 +0,0 @@
-@file:JvmName("NamedStackFrameKotlin")
-package org.jonnyzzz.stack
-
-fun <T> NamedStackFrame.wrap(name: String, action: () -> T) : T {
-  return executor(name).__(FrameFunction<T, Throwable> { action() })
-}
-
