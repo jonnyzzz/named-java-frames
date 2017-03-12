64b72c3b29571d83f9e202add44578a86b142dbc
Eugene Petrenko <eugene.petrenko@gmail.com>
2017-03-12 12:00:45 +0100

    hot-fix filing tests (with the cost of extra stack frame)

--- named-frames-kotlin/src/main/java/org/jonnyzzz/stack/KotlinExtension.kt
+++ named-frames-kotlin/src/main/java/org/jonnyzzz/stack/KotlinExtension.kt
@@ -1,7 +1,7 @@
 @file:JvmName("NamedStackFrameKotlin")
 package org.jonnyzzz.stack
 
-inline fun <T> NamedStackFrame.wrap(name: String, crossinline action: () -> T) : T {
+fun <T> NamedStackFrame.wrap(name: String, action: () -> T) : T {
   return executor(name).__(NamedStackFrame.FrameFunction<T, Throwable> { action() })
 }
 
